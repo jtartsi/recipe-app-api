b679169ad0805f916529fc7242931a023de699fe
Jani Tarkiainen <j.tartsi@gmail.com>
2020-02-04 17:29:51 +0700

    Added custom user model

---
+++ devenv.sh
@@ -0,0 +1,19 @@
+fun_run_tests() {
+  docker-compose run app sh -c "python manage.py test"
+}
+
+fun_run_tests_and_lint() {
+  docker-compose run app sh -c "python manage.py test && flake8"
+}
+
+fun_build_docker() {
+  docker-compose build
+}
+
+fun_create_core_app() {
+  docker-compose run app sh -c "python manage.py startapp core"
+}
+
+fun_make_core_migrations() {
+  docker-compose run app sh -c "python manage.py makemigrations core"
+}
