84b915b8e8a3c0c825dde704715793e3472aeb05
Jani Tarkiainen <j.tartsi@gmail.com>
2020-02-05 14:16:15 +0700

    Added endpoint for creating and authenticating users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,7 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.test import Client
 
 
 class AdminSiteTests(TestCase):
@@ -9,18 +8,18 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@londonappdev.com',
+            email='admin@jmitesolutions.com',
             password='password123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='test@londonappdev.com',
+            email='test@jmitesolutions.com',
             password='password123',
-            name='Test User Full Name',
+            name='John Doe'
         )
 
     def test_users_listed(self):
-        """Test that users are listed on the user page"""
+        """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
@@ -38,13 +37,6 @@ class AdminSiteTests(TestCase):
     def test_create_user_page(self):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
-        res = self.client.get(url)
-
-        self.assertEqual(res.status_code, 200)
-
-    def test_user_page_change(self):
-        """Test that the user edit page works"""
-        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
