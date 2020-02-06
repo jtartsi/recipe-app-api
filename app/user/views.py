7ae7fecc33fa2227dd42b64510e32300fd3e40d2
Jani Tarkiainen <j.tartsi@gmail.com>
2020-02-06 14:17:13 +0700

    Added manage user endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
-# TODO above is not working with the intellisense
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -14,3 +13,13 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
